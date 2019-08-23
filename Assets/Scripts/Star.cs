fb76a3c850f0ae62b8f4b9dff11a6120d7e33e87
Andreas Garvik <andreas.garvik13@gmail.com>
2019-08-23 21:10:19 +1000

    Initial commit

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+	public Material Material;
+
+	private readonly Vector3[] Vertices = {
+		new Vector3(-1, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0.5f, 0),
+		new Vector3(0,-1, 0), new Vector3(0, 0, 0), new Vector3(-0.5f, 0, 0),
+		new Vector3(1, 0, 0), new Vector3(0, 0, 0), new Vector3(0, -0.5f, 0),
+		new Vector3(0, 1, 0), new Vector3(0, 0, 0), new Vector3(0.5f, 0, 0)
+	};
+
+	void Start()
+	{
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+		GetComponent<MeshRenderer>().material = Material;
+
+		mesh.Clear();
+
+		mesh.vertices = Vertices;
+
+		mesh.colors = new Color[]
+		{
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+						new Color(1.0f, 1.0f, 1.0f, 1.0f),
+		};
+
+		mesh.triangles = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+	}
+
+	void Update()
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+
+	}
+}
+
---
+++ Assets/Scripts/StarRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRenderer : MonoBehaviour
+{
+
+	public GameObject star;
+	private Star[] stars = new Star[2];
+	void Start()
+	{
+		for (int i = 0; i < stars.Length; i++)
+		{
+			Star s = Instantiate(star).GetComponent<Star>();
+			stars[i] = s;
+
+		}
+	}
+	void Update()
+	{
+		Vector3 v1 = new Vector3();
+		Vector3 v2 = new Vector3();
+		v1.x = stars[0].transform.position.x + -0.1f * Time.deltaTime;
+		v2.x = stars[1].transform.position.x + 0.1f * Time.deltaTime;
+		stars[0].transform.position = v1;
+		stars[1].transform.position = v2;
+	}
+}
